2de5ff2feb0eba6a426b7f94ecba788e1754011c
borgesjose <jose.borges90@hotmail.com>
2022-05-22 21:58:25 -0300

    22-05-2022 version

---
+++ MATLAB/FUZZY-TYPE1-NONLINEAR-TANK/tank_conical.m
@@ -0,0 +1,12 @@
+function dh_dt = tank_conical(t,y,A,Qin,Cd,R1,R2)
+    % calculate derivative of the Level
+    
+    g = 9.81;
+    H = 0.3; % Altura
+    %R1 = 0.20; % Raio em metros
+    %R2 = 0.01; % Raio da base do cilindro
+    
+    b = A*Cd*sqrt(2*g);
+
+    dh_dt = (Qin - b*y^(1/2))/(R2 + ((R1-R2)/H)*y)^(2);
+end
