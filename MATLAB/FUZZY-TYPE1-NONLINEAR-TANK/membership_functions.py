9e6c3dbd456cef17f7e92d2a484e1b08cd2b982e
borgesjose <jose.borges90@hotmail.com>
2022-05-29 21:58:14 -0300

    29-05-2022 version

--- MATLAB/FUZZY-TYPE1-NONLINEAR-TANK/membership_functions.py
+++ MATLAB/FUZZY-TYPE1-NONLINEAR-TANK/membership_functions.py
@@ -1,3 +1,15 @@
 def singleton_t1(c,x):
     if x==c: y = 1;
     else: y=0;
+
+    return y
+
+def tri_mf_t1(x,param):
+    a = param[0];
+    b = param[1];
+    c = param[2];  
+
+    y = max(min((x-a)/(b-a),(c-x)/(c-b)),0);
+    
+    return y
+
