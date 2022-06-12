d0e2ea0bc2988471fec5762f20ad22b51be46945
borgesjose <jose.borges90@hotmail.com>
2022-06-12 09:24:23 -0300

    12-06-2022 version

--- MATLAB/FUZZY-TYPE2-NONLINEAR-TANK/IG.m
+++ MATLAB/FUZZY-TYPE2-NONLINEAR-TANK/IG.m
@@ -1,13 +1,14 @@
 function ig = IG(a1,a2,a3,u,r,e),
 
 
-e1 = 
+e1 = sum(abs(u'))/size(u,1)
+e11 = (u'*u)%/size(u,1)
 
-e2 = 
+e2 = 0
 
-e3 = 
+e3 = 0
 
 
-ig = a1*e1+a2*e2+a*e3;
+ig = a1*e1+a2*e2+a3*e3;
 
 end 
